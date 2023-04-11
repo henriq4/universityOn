283bfe2b474564fee878aac6c1740d2eed6d83d9
Henrique GC <henriq4NET@gmail.com>
2023-04-11 19:00:54 -0300

    feat(java): create first class exercises - initial configuration

---
+++ activities/Java/class_1/lamp.java
@@ -0,0 +1,25 @@
+package class_1;
+
+class Lamp {
+  private boolean state;
+
+  Lamp() {
+    this.state = false;
+  }
+
+  Lamp(boolean state) {
+    this.state = state;
+  }
+
+  void turnOn() {
+    this.state = true;
+  }
+
+  void turnOff() {
+    this.state = false;
+  }
+
+  String showState() {
+    return this.state ? "Acesa" : "Apagada";
+  }
+}
