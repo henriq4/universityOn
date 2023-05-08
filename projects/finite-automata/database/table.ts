86a70b42c539fa3ea8f899f0d1b6780552be4102
Henrique GC <henriq4NET@gmail.com>
2023-05-08 11:57:55 -0300

    chore(projects/finite-automata): creating and apply alphabet in automata

--- projects/finite-automata/database/table.ts
+++ projects/finite-automata/database/table.ts
@@ -3,40 +3,43 @@ let table = new Map();
 let initial_w = "q0"
 let final_w = "q2"
 
+let alphabet = ["a", "b"]
+
 table.set(
   "q0",
   new Map([
-    ["a", "q1"],
-    ["b", "q3"],
+    [alphabet[0], "q1"],
+    [alphabet[1], "q3"],
   ])
 );
 
 table.set(
   "q1",
   new Map([
-    ["a", "q2"],
-    ["b", "q3"],
+    [alphabet[0], "q2"],
+    [alphabet[1], "q3"],
   ])
 );
 
 table.set(
   "q2",
   new Map([
-    ["a", "q2"],
-    ["b", "q2"],
+    [alphabet[0], "q2"],
+    [alphabet[1], "q2"],
   ])
 );
 
 table.set(
   "q3",
   new Map([
-    ["a", "q1"],
-    ["b", "q2"],
+    [alphabet[0], "q1"],
+    [alphabet[1], "q2"],
   ])
 );
 
 export {
   table,
+  alphabet,
   initial_w,
   final_w
 };
--- projects/finite-automata/index.ts
+++ projects/finite-automata/index.ts
@@ -1,11 +1,11 @@
 import process from "node:process";
 
-import {table, final_w, initial_w} from "./table";
+import { table, final_w, initial_w, alphabet } from "./database/table";
 
 const encoded = process.argv[2];
 
 if (!encoded) {
-  console.log("not informed word");
+  console.log("any word was informed... exiting");
   process.exit(1);
 }
 
@@ -17,15 +17,12 @@ function delta(w: string, state: string): any {
   let letter: string = w[0];
   let newState = table.get(state).get(letter);
 
-  switch (letter) {
-    case "a":
-      return delta(w.slice(1), newState);
-    case "b":
-      return delta(w.slice(1), newState);
-    default:
-      console.log("error");
-      process.exit(2);
+  if (alphabet.includes(letter)) {
+    return delta(w.slice(1), newState);
   }
+
+  console.log("exception: letter don't containg in alphabet");
+  process.exit(2);
 }
 
 const f_state = delta(encoded, initial_w);
