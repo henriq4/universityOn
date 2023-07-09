0fc2aea4c8d0c95de55c8144d9f8892fdd64c40e
Henrique GC <henriq4net@gmail.com>
2023-07-09 19:43:54 -0300

    chore(class): moving determinist-automato basic implementation to class

--- class/computation_theory/determinist-finite-automata/index.ts
+++ class/computation_theory/determinist-finite-automata/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
 import process from "node:process";
 
 import { table, final_w, initial_w, alphabet } from "./database/table";
@@ -14,19 +16,19 @@ function delta(w: string, state: string): any {
     return state;
   }
 
-  let letter: string = w[0];
-  let newState = table.get(state).get(letter);
+  const letter: string = w[0];
+  const newState = table.get(state).get(letter);
 
   if (alphabet.includes(letter)) {
     return delta(w.slice(1), newState);
   }
 
   console.log("exception: letter don't containg in alphabet");
-  process.exit(2);
+  return process.exit(2);
 }
 
 const f_state = delta(encoded, initial_w);
 
 f_state === final_w
-  ? console.log("match! final state: " + f_state)
-  : console.log("don't match, final state: " + f_state)
+  ? console.log(`match! final state: ${f_state}`)
+  : console.log(`don't match, final state: ${f_state}`);
