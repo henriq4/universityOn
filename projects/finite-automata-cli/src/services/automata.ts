2d12aa3d0999999312ddc15b8bbfc1d5beb79195
Henrique GC <henriq4net@gmail.com>
2023-08-19 21:06:10 -0300

    chore(projects/finite-automata): adding time milesconds calculus

--- projects/finite-automata-cli/src/services/automata.ts
+++ projects/finite-automata-cli/src/services/automata.ts
@@ -1,18 +1,18 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable no-restricted-syntax */
 import { AutomataModel } from "../models";
 import { createStateName } from "../utils/createStateName";
 
 export class Automata {
   constructor(private automata: AutomataModel) {}
 
-  delta(state: number, symbol: string): number {
+  delta(state: number, symbol: string): number[] | -1 {
     const key = createStateName(state, symbol);
 
-    if (this.automata.transitions.has(key)) {
-      return this.automata.transitions.get(key)!;
+    if (this.automata.transitions.has(key) === false) {
+      return -1;
     }
 
-    return -1;
+    return this.automata.transitions.get(key)!;
   }
 
   extendedTransition(state: number, input: string): number {
@@ -22,17 +22,38 @@ export class Automata {
 
     const letter: string = input[0];
 
-    const nextState = this.delta(state, letter);
+    const nextStates = this.delta(state, letter);
 
-    if (nextState !== -1) {
-      return this.extendedTransition(nextState, input.slice(1));
+    if (nextStates === -1) return -1;
+
+    for (const nextState of nextStates) {
+      if (input.length === 1) {
+        for (const r of nextStates) {
+          if (this.automata.final.has(r) === true) {
+            return r;
+          }
+        }
+      }
+
+      const result = this.extendedTransition(nextState, input.slice(1));
+      if (result !== -1) {
+        return result;
+      }
     }
 
     return -1;
   }
 
-  run(input: string): 0 | 1 {
+  run(input: string): { obtained: 0 | 1; timeMilliseconds: number } {
+    const startTime = process.hrtime();
     const finalState = this.extendedTransition(this.automata.initial, input);
-    return this.automata.final.has(finalState) === true ? 1 : 0;
+
+    const endTime = process.hrtime(startTime);
+    const elapsedTimeInMs = endTime[0] * 1000 + endTime[1] / 1000000;
+
+    return {
+      obtained: this.automata.final.has(finalState) === true ? 1 : 0,
+      timeMilliseconds: elapsedTimeInMs,
+    };
   }
 }
