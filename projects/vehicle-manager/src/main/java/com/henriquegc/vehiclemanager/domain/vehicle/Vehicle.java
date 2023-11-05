76adabda5e711e9eaacc2348d545bcca22b3bdf4
Henrique GC <henriq4net@gmail.com>
2023-11-05 17:12:13 -0300

    feat(vehicle-manager): making vehicle a database entity

--- projects/vehicle-manager/src/main/java/com/henriquegc/vehiclemanager/domain/vehicle/Vehicle.java
+++ projects/vehicle-manager/src/main/java/com/henriquegc/vehiclemanager/domain/vehicle/Vehicle.java
@@ -1,9 +1,60 @@
 package com.henriquegc.vehiclemanager.domain.vehicle;
 
+import javax.persistence.*;
+
+@Entity
 public class Vehicle {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+
+  @Column
   private String brand;
 
+  @Column
   private String model;
 
+  @Column(unique = true)
   private String plate;
+
+  public Vehicle() {}
+
+  public Vehicle(String brand, String model, String plate) {
+    this.brand = brand;
+    this.model = model;
+    this.plate = plate;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public String getPlate() {
+    return plate;
+  }
+
+  public void setPlate(String plate) {
+    this.plate = plate;
+  }
 }
