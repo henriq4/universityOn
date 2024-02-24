e221f64cff1933bee7d2b47401f27cd261cd1550
Henrique GC <henriq4NET@gmail.com>
2024-02-24 00:08:21 -0300

    fix(turing-machine): removing target folder

--- projects/turing-machine/target/debug/deps/turing_machine-1fb607837310f05b.d
+++
@@ -1,5 +0,0 @@
-/home/henriq4/code/universityOn/projects/turing-machine/target/debug/deps/turing_machine-1fb607837310f05b: src/main.rs
-
-/home/henriq4/code/universityOn/projects/turing-machine/target/debug/deps/turing_machine-1fb607837310f05b.d: src/main.rs
-
-src/main.rs:
--- projects/turing-machine/target/debug/turing-machine.d
+++
@@ -1,1 +0,0 @@
-/home/henriq4/code/universityOn/projects/turing-machine/target/debug/turing-machine: /home/henriq4/code/universityOn/projects/turing-machine/src/main.rs
